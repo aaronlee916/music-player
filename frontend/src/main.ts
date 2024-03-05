332a05ff64087758a52c06fb62459f04c5b46f92
Aaron Li <aaronlee916@qq.com>
2024-03-05 11:07:58 +0800

    fixed sass

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './uni.promisify.adaptor'
+import Vue from "vue";
+import uView from "uview-ui";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import "./uni.promisify.adaptor";
 
-const app = new (typeof App === 'function' ? App : Vue.extend(Object.assign({ mpType: 'app' }, App)))
+Vue.use(uView);
+Vue.config.productionTip = false;
+
+const app = new (
+  typeof App === "function"
+    ? App
+    : Vue.extend(Object.assign({ mpType: "app" }, App))
+)();
 app.$mount();
--- frontend/src/pages/index/index.vue
+++ frontend/src/pages/index/index.vue
@@ -1,51 +1,31 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+  <view>
+    <u-navbar title="个人中心" @rightClick="rightClick" :autoBack="true">
+    </u-navbar>
+  </view>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
+import Vue from "vue";
 
-	export default Vue.extend({
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+export default Vue.extend({
+  data() {
+    return {
 
-		},
-		methods: {
-
-		}
-	});
+    };
+  },
+  onLoad() {},
+  methods: {
+    rightClick() {
+      console.log("rightClick");
+    },
+    leftClick() {
+      console.log("leftClick");
+    },
+  },
+});
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
